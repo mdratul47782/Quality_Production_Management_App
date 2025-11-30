b9adf850944851c13140969a2112bef339909884
mdratul47782 <mdratul47782@gmail.com>
2025-11-30 10:48:50 +0600

    Updated

--- services/mongo.js
+++ services/mongo.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export async function dbConnect() {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log("Connected");
+    console.log("Connected.");
     return conn;
   } catch (err) {
     console.log(err);
